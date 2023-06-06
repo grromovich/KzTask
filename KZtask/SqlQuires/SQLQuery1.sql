8b9b6b3de133b47d7b845488207ab5a629f6ad1b
grromovich <Legendaryperson@yandex.ru>
2023-06-06 08:24:56 +0300

    Исправил sql запрос

--- KZtask/SqlQuires/SQLQuery1.sql
+++ KZtask/SqlQuires/SQLQuery1.sql
@@ -15,14 +15,14 @@ Text nvarchar(255) NOT NULL
 INSERT INTO [Users] VALUES ('admin', 'admin');
 INSERT INTO [Users] VALUES ('user', 'user');
 
-INSERT INTO [Tasks] (Id, Date_month, Text) VALUES ('admin', '05/06/2023', N'Яблоко');
-INSERT INTO [Tasks] (Id, Date_month, Text) VALUES ('admin', '03/06/2023', N'Банан');
-INSERT INTO [Tasks] (Id, Date_month, Text) VALUES ('admin', '02/06/2023', N'Груша');
-INSERT INTO [Tasks] (Id, Date_month, Text) VALUES ('admin', '01/06/2023', N'Персик');
+INSERT INTO [Tasks] (Id, Date_month, Text) VALUES ('admin', '05/06/2023', N'Значимость этих проблем настолько очевидна, что начало повседневной');
+INSERT INTO [Tasks] (Id, Date_month, Text) VALUES ('admin', '03/06/2023', N'Товарищи! укрепление и развитие');
+INSERT INTO [Tasks] (Id, Date_month, Text) VALUES ('admin', '02/06/2023', N'Задача организации, в особенности же дальнейшее развитие различных форм деятельности представляет собой интересный');
+INSERT INTO [Tasks] (Id, Date_month, Text) VALUES ('admin', '01/06/2023', N'Таким образом постоянное информационно-пропагандистское обеспечение нашей деятельности');
 
-INSERT INTO [Tasks] (Id, Date_month, Text) VALUES ('user', '05/06/2023', N'Апельсин');
-INSERT INTO [Tasks] (Id, Date_month, Text) VALUES ('user', '02/06/2023', N'Ананас');
-INSERT INTO [Tasks] (Id, Date_month, Text) VALUES ('user', '01/06/2023', N'Киви');
+INSERT INTO [Tasks] (Id, Date_month, Text) VALUES ('user', '05/06/2023', N'Таким образом сложившаяся структура организации представляет');
+INSERT INTO [Tasks] (Id, Date_month, Text) VALUES ('user', '02/06/2023', N'Задача организации, в особенности же постоянный количественный рост ');
+INSERT INTO [Tasks] (Id, Date_month, Text) VALUES ('user', '01/06/2023', N'Разнообразный и богатый опыт укрепление и развитие структуры');
 
 SELECT * FROM [Users];
 SELECT * FROM [Tasks];
